dccd5487d903366f3e598801f9244e833d1522f1
Jan Willem Henckel <farly.de@gmail.com>
2022-09-26 14:35:32 +0200

    fix show full date instead of only the year

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const yearElement = document.querySelector('[data-js="year"]');
-const year = new Date().getFullYear();
-yearElement.textContent = year;
+const dateElement = document.querySelector('[data-js="date"]');
+const date = new Date().toLocaleDateString("en-US");
+dateElement.textContent = date;
